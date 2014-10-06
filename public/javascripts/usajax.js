f9fac6c9235c88490f8d0a20a15b3611698bbbe8
stbarrientos <stbarrientos@gmail.com>
2014-10-06 13:39:54 -0400

    successful ajax call

---
+++ public/javascripts/usajax.js
@@ -0,0 +1,10 @@
+var apiKey = "hequvx2dwyfntbhae7b769hr";
+var zipcode = "Great Falls";
+$.ajax({
+	url: "http://api.usatoday.com/open/census/loc?keypat=" + zipcode + "&keyname=placename&sumlevid=4,6&api_key=" + apiKey,
+	method: "GET",
+	dataType: "jsonp",
+	success: function(data){
+		console.log(data);
+	}
+});
--- server.rb
+++ server.rb
@@ -1,5 +1,8 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require_relative 'secrets.rb'
+
+@usa_today = Key.usa_today
 
 get '/' do
 	@message = "Hello"
